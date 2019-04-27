453fcf78d064017ddce8379d33b483b6d8616c2c
mouhoub bouhoum <mouhoub@mouhoubs-MacBook-Air.local>
2019-04-27 13:30:42 -0500

    third

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path= require('path');
 
 const mongoose = require("mongoose");
 const routes = require("./routes");
@@ -11,10 +12,13 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", 'index.html'))
+})
 app.use(routes);
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
+  process.env.MONGODB_URI || "mongodb://booksearch:a123456@ds149146.mlab.com:49146/heroku_7z7slkq5"
 );
 
 app.listen(PORT, function() {
